d79e385b3043074977b86e3a0f094e6b6aa6685c
mstetsyuk <m.stetsyuk@togas.com>
2023-07-09 22:19:31 +0300

    Create sumSalary function

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,14 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  let salariesValues = Object.values(salaries).filter((val) => {
+    if (!isNaN(val) && val !== Infinity && val !== -Infinity) {
+      return typeof val == 'number';
+    }
+  });
+
+  for (let salary of salariesValues) {
+    sum += salary;
+  }
+
+  return sum;
 }
